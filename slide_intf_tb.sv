236610614fe63ef194dd56e225fd26c201d91b80
afisher92 <fisheand92@gmail.com>
2015-11-19 17:51:16 -0600

    Update slide_intf_tb.sv

--- slide_intf_tb.sv
+++ slide_intf_tb.sv
@@ -3,6 +3,7 @@ module slide_intf_tb();
 reg clk, rst_n, MISO;
 
 wire SCLK, a2d_SS_n;
+wire [2:0] nxtcnt;
 wire [11:0] POT_LP, POT_B1, POT_B2, POT_B3, POT_HP, VOLUME;
 
 slide_intf iDUT(.clk(clk), .cnv_cmplt(cnv_cmplt), .rst_n(rst_n), .chnnl(chnnl), .strt_cnv(strt_cnv), .POT_LP(POT_LP), .POT_B1(POT_B1), .POT_B2(POT_B2), .POT_B3(POT_B3), .POT_HP(POT_HP), .VOLUME(VOLUME));
@@ -11,15 +12,16 @@ initial begin
 clk = 1'b0;
 rst_n = 1'b0;
 MISO = 1'b0;
-#5
+#20
 rst_n = 1'b1;
 MISO = 1'b1;
-
-#5
-MISO = 1'b1;
+#140
+rst_n = 1'b0;
+#40
+rst_n = 1'b1;
 end
 
 always begin
- #5 clk = ~clk;
+ #10 clk = ~clk;
 end
 endmodule
